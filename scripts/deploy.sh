5690b42eaac865c9fc496f6417f796b0c20e0888
Maslamah <turkmani1439@gmail.com>
2026-09-09 18:48:16 +0100

    all scripts finished

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+cd /Users/maslamah/monitoring-stack
+docker-compose up --build -d
+
+# Until curl returns 0 the waiting message will be shown
+
+until curl -sf http://localhost:8080/app-a/ > /dev/null; do
+    echo "Waiting for App A"
+    sleep 2
+done
+echo "App A is working"
+
+until curl -sf http://localhost:8080/app-b/ > /dev/null; do
+    echo "Waiting for App B"
+    sleep 2
+done
+echo "App B is working"
---
+++ scripts/healthcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+LOGFILE="/Users/maslamah/monitoring-stack/HealthCheckLogs"
+timestamp=$(date)
+
+echo " $timestamp - App check " >> "$LOGFILE"
+# if curl returns 0 the first command will run
+if curl -sf http://localhost:8080/app-a/ > /dev/null; then
+    echo " $timestamp - App A is working " >> "$LOGFILE"
+else 
+    echo " $timestamp - App A is DOWN " >> "$LOGFILE"
+fi
+
+if curl -sf http://localhost:8080/app-b/ > /dev/null; then
+    echo " $timestamp - App B is working " >> "$LOGFILE"
+else   
+    echo " $timestamp - App B is DOWN " >> "$LOGFILE"
+    
