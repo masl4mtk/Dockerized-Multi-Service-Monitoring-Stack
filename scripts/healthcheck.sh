f74d0ed17beba8120d468aac92d1e84cf105721d
Maslamah <turkmani1439@gmail.com>
2026-09-10 16:07:58 +0100

    script change

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -2,18 +2,23 @@
 
 set -euo pipefail
 
-LOGFILE=("$(dirname "{$BASH_SOURCE[0]}")/../HealthCheckLogs")
+LOGFILE="$(dirname "${BASH_SOURCE[0]}")/../HealthCheckLogs.log"
 timestamp=$(date)
+STATUS=0
 
-# if curl returns 0 the first command will run
+
 if curl -sf http://localhost:8080/app-a/ > /dev/null; then
     echo " $timestamp - App A is working " >> "$LOGFILE"
 else 
     echo " $timestamp - App A is DOWN " >> "$LOGFILE"
+    STATUS=1
 fi
 
 if curl -sf http://localhost:8080/app-b/ > /dev/null; then
     echo " $timestamp - App B is working " >> "$LOGFILE"
 else   
     echo " $timestamp - App B is DOWN " >> "$LOGFILE"
+    STATUS=1
 fi
+
+exit $STATUS
